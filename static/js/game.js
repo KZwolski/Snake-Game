e365bce60aa7bfd7c8938954dfe41caa7b48d397
Sylwester <tomczyk.sylwester@wp.pl>
2022-05-31 09:37:51 +0200

    add fruit on map

--- static/js/game.js
+++ static/js/game.js
@@ -1,20 +1,14 @@
 initGame();
 
 function initGame() {
-    alert('script working')
     generateFruit()
-    // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
 
 function generateFruit() {
-    let map = document.querySelector('#map')
-    // To prevent generating food over the snake
-    // while (map.children[food].classList.contains("snake-body")) {
-    //     food = Math.floor(Math.random() * 100);
-    // }
-    // Placing food on the map
+    let board = document.querySelector('#game_board')
     let newFruit = document.createElement('div')
-    map.append(newFruit)
-    map.children[0].classList.add("fruit")
+    newFruit.classList.add("fruit")
+    board.append(newFruit)
+    // console.log(Math.floor(Math.random() * 11))
 }
